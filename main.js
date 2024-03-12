0282e8b45e1ea172ce1c2b5556e82fdd6d03e756
Mukhammadaziz <rustamov977075073@gmail.com>
2024-03-12 11:44:17 +0500

    7-dars

---
+++ main.js
@@ -0,0 +1,39 @@
+// let res = arr.length;
+// let res = arr.toString();
+// let res = arr.join(' A ');
+// let res = arr.at(2);
+// let res = arr.pop();
+// let res = arr.push('Mars IT school');
+// let res = arr.unshift('Mars');
+// delete arr[3];
+// let res = arr.concat(arr2);
+
+// let arr = ['aziz', 'bosit', 'fozil', 'azim', 'said'];
+// let res = confirm();
+
+// if (res === true){
+//     console.log(arr.splice(0,2));    
+// } else {
+//     console.log(arr.splice(2,5));
+// }
+
+// let arr = ['aziz', 'bosit', 'fozil', 'azim', 'said'];
+// let res = confirm()
+
+// if (res === true){
+//     console.log(arr.shift());
+// } else {
+//     console.log(arr.pop(5));
+// }
+
+// console.log(arr);
+
+let a = ['Muhammadaziz', 'Fozil', 'Azim','Bosit'];
+let b = confirm(a);
+
+if (b === true) {
+    a.shift('Bosit');
+    console.log(a);
+} else {
+    console.log(a);
+}
